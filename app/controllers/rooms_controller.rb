a2f5592543fb2e5cc82fd3955f9130da78069199
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-10-28 17:13:09 -0300

    add skip do login na página de show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     if params[:keyword].present?
