7e3767865b933cb04ce183243cd9d25aec64d05d
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-10-29 17:18:55 -0300

    --

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,44 +1,10 @@
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import places from 'places.js';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const markers = JSON.parse(mapElement.dataset.markers);
-
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-
-    const fitMapToMarkers = (map, markers) => {
-      const bounds = new mapboxgl.LngLatBounds();
-      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-    };
-
-    const addMarkersToMap = (map, markers) => {
-      markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-        const element = document.createElement('div');
-        element.className = 'marker';
-        element.style.backgroundImage = `url('${marker.image_url}')`;
-        element.style.backgroundSize = 'contain';
-        element.style.width = '25px';
-        element.style.height = '25px';
-        new mapboxgl.Marker(element)
-          .setLngLat([marker.lng, marker.lat])
-          .setPopup(popup)
-          .addTo(map);
-      });
-    }
-
-
-    fitMapToMarkers(map, markers);
-    addMarkersToMap (map, markers);
+const initAutocomplete = () => {
+  const addressInput = document.getElementById("room_address");
+  if (addressInput) {
+    places({ container: addressInput });
   }
 };
 
-export { initMapbox };
+export { initAutocomplete };
